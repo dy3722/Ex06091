6cc54a8bd868ef06bc173b0a1a6845f8481845d6
dy3722 <dy3722@bs.amalnet.k12.il>
2025-04-17 17:51:34 +0300

    end of design

--- app/src/main/java/com/example/ex06091/MainActivity.java
+++ app/src/main/java/com/example/ex06091/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex06091;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +15,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText et;
+    Button btn;
+    WebView wv;
+    String url;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et = findViewById(R.id.et);
+        btn = findViewById(R.id.btn);
+        wv = findViewById(R.id.wv);
+        url = "";
+
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setWebViewClient(new MyWebViewClient());
+    }
+
+    public void go(View view) {
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        public boolean shouldOverideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
